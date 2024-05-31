ffe358c111fe35ebe553c0a3778bd6be6be935cd
BrandonFigueroa <brandonf2345@gmail.com>
2024-05-30 20:27:16 -0600

    create employeeDto and EmployeeMapper

---
+++ ems-backend/src/main/java/net/btech/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package net.btech.ems.mapper;
+
+import net.btech.ems.dto.EmployeeDto;
+import net.btech.ems.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
